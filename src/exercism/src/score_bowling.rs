6bf2506c9b105d0ab8cb5b1ec5bfcc72ada71b81
winter.loo <adjkldd@126.com>
2025-03-10 11:05:03 +0800

    learn from othrers' bowling score idea

--- src/exercism/src/score_bowling.rs
+++ src/exercism/src/score_bowling.rs
@@ -2,7 +2,6 @@
 //!
 //! https://exercism.org/tracks/rust/exercises/bowling
 //!
-/// key to success: thinking in small and solvable steps
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
@@ -10,210 +9,280 @@ pub enum Error {
     GameComplete,
 }
 
-#[derive(Copy, Clone)]
-struct Frame {
-    pins: [u16; 2],
-    n: u16,
-}
-
-impl Frame {
-    fn new() -> Self {
-        Self { pins: [0; 2], n: 0 }
-    }
-
-    fn is_strike(&self) -> bool {
-        self.pins[0] == 10
-    }
-
-    fn is_open(&self) -> bool {
-        self.pins[0] < 10 && self.pins[0] + self.pins[1] < 10
-    }
-
-    fn is_spare(&self) -> bool {
-        self.pins[0] < 10 && self.pins[0] + self.pins[1] == 10
-    }
-
-    fn record(&mut self, pins: u16) -> Result<bool, Error> {
-        if self.is_strike() {
-            Ok(false)
-        } else if self.n < 2 {
-            if self.n == 0 || self.pins[0] + pins <= 10 {
-                self.pins[self.n as usize] = pins;
-            } else {
-                return Err(Error::NotEnoughPinsLeft);
-            }
-            self.n += 1;
-            Ok(true)
-        } else {
-            Ok(false)
-        }
-    }
-
-    fn score(&self) -> u16 {
-        self.pins.iter().sum()
-    }
-
-    fn score_of_first_roll(&self) -> u16 {
-        self.pins[0]
-    }
-
-    fn end(&self) -> bool {
-        self.n == 2 || self.is_strike()
-    }
-}
-
-struct FinalFrame {
-    frame: Frame,
-    extra_rolls: [u16; 2],
-    n: u8,
-    extra: i8,
-}
-
-impl FinalFrame {
-    fn new() -> Self {
-        FinalFrame {
-            frame: Frame::new(),
-            extra_rolls: [0; 2],
-            n: 0,
-            extra: -1,
-        }
-    }
-
-    fn record(&mut self, pins: u16) -> Result<(), Error> {
-        if self.extra == -1 {
-            let succ = self.frame.record(pins);
-            match succ {
-                Err(e) => return Err(e),
-                Ok(true) => return Ok(()),
-                Ok(false) => {}
-            }
-
-            if self.frame.is_strike() {
-                self.extra = 2;
-            } else if self.frame.is_spare() {
-                self.extra = 1;
-            } else {
-                self.extra = 0;
-            }
-        }
-
-        if self.n < self.extra as u8 {
-            if self.n == 0 {
-                self.extra_rolls[self.n as usize] = pins;
-            } else if self.n == 1 {
-                if self.extra_rolls[0] == 10 || self.extra_rolls[0] + pins <= 10 {
-                    self.extra_rolls[1] = pins;
-                } else {
-                    return Err(Error::NotEnoughPinsLeft);
-                }
-            }
-            self.n += 1;
-            Ok(())
-        } else {
-            Err(Error::GameComplete)
-        }
-    }
-
-    fn score(&self) -> u16 {
-        self.frame.score()
-            + if self.extra > 0 {
-                self.extra_rolls[..self.extra as usize].iter().sum::<u16>()
-            } else {
-                0
-            }
-    }
-
-    fn score_of_first_roll(&self) -> u16 {
-        self.frame.score_of_first_roll()
-    }
-
-    fn score_of_first_two_rolls(&self) -> u16 {
-        if self.frame.is_strike() {
-            self.frame.score() + self.extra_rolls[0]
-        } else {
-            self.frame.score()
-        }
-    }
-
-    fn game_over(&self) -> bool {
-        self.n == self.extra as u8 || self.frame.end() & self.frame.is_open()
-    }
-}
-
+/// key idea from : https://exercism.org/tracks/rust/exercises/bowling/solutions/potatosalad
+/// basic thinking model:
+/// put all pins in a Vec and then use variables to partition this Vec.
 pub struct BowlingGame {
-    frames: [Frame; 9],
-    tenth_frame: FinalFrame,
-    n: u8,
+    rolls: Vec<u16>,
+    second_throw_in_frame: bool,
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
-        Self {
-            frames: [Frame::new(); 9],
-            tenth_frame: FinalFrame::new(),
-            n: 0,
+        BowlingGame {
+            rolls: vec![],
+            // key design
+            second_throw_in_frame: false,
         }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        if pins > 10 {
+        if pins > 10 || self.second_throw_in_frame && self.rolls.last().unwrap() + pins > 10 {
             return Err(Error::NotEnoughPinsLeft);
+        } else if self.score().is_some() {
+            return Err(Error::GameComplete);
         }
-        if self.n == 9 {
-            self.tenth_frame.record(pins)?;
+
+        self.rolls.push(pins);
+        self.second_throw_in_frame = if pins == 10 {
+            false
         } else {
-            match self.frames[self.n as usize].record(pins) {
-                Ok(false) => {
-                    self.n += 1;
-                    if self.n == 9 {
-                        self.tenth_frame.record(pins);
-                    } else {
-                        self.frames[self.n as usize].record(pins);
-                    }
-                }
-                Err(e) => return Err(e),
-                Ok(true) => {}
-            }
-        }
+            !self.second_throw_in_frame
+        };
+
         Ok(())
     }
 
+    /// basic thinking model: if we can not get the final score, the game is not over.
     pub fn score(&self) -> Option<u16> {
-        if self.tenth_frame.game_over() {
-            let mut score = 0;
-            for (i, fr) in self.frames.iter().enumerate() {
-                score += fr.score();
-                if fr.is_strike() {
-                    if i + 1 == 9 {
-                        score += self.tenth_frame.score_of_first_two_rolls();
+        let mut total = 0;
+        let mut n = 0;
+        for _ in 0..10 {
+            // key observation
+            if let (Some(&first), Some(&second)) = (self.rolls.get(n), self.rolls.get(n + 1)) {
+                total += first + second;
+                if first == 10 || first + second == 10 {
+                    if let Some(third) = self.rolls.get(n + 2) {
+                        total += third;
                     } else {
-                        let next_frame = self.frames[i + 1];
-                        score += next_frame.score();
-                        if next_frame.is_strike() {
-                            if i + 2 == 9 {
-                                score += self.tenth_frame.score_of_first_roll();
-                            } else {
-                                let next_frame = self.frames[i + 2];
-                                score += next_frame.score_of_first_roll();
-                            }
-                        }
-                    }
-                } else if fr.is_spare() {
-                    if i + 1 == 9 {
-                        score += self.tenth_frame.score_of_first_two_rolls();
-                    } else {
-                        let next_frame = self.frames[i + 1];
-                        score += next_frame.score_of_first_roll();
+                        return None;
                     }
                 }
+                // key design
+                n += if first == 10 { 1 } else { 2 };
+            } else {
+                return None;
             }
-            score += self.tenth_frame.score();
-            Some(score)
-        } else {
-            None
         }
+        Some(total)
     }
 }
+
+/// initia design
+/// key to success: thinking in small and solvable steps
+mod v0 {
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub enum Error {
+        NotEnoughPinsLeft,
+        GameComplete,
+    }
+
+    #[derive(Copy, Clone)]
+    struct Frame {
+        pins: [u16; 2],
+        n: u16,
+    }
+
+    impl Frame {
+        fn new() -> Self {
+            Self { pins: [0; 2], n: 0 }
+        }
+
+        fn is_strike(&self) -> bool {
+            self.pins[0] == 10
+        }
+
+        fn is_open(&self) -> bool {
+            self.pins[0] < 10 && self.pins[0] + self.pins[1] < 10
+        }
+
+        fn is_spare(&self) -> bool {
+            self.pins[0] < 10 && self.pins[0] + self.pins[1] == 10
+        }
+
+        fn record(&mut self, pins: u16) -> Result<bool, Error> {
+            if self.is_strike() {
+                Ok(false)
+            } else if self.n < 2 {
+                if self.n == 0 || self.pins[0] + pins <= 10 {
+                    self.pins[self.n as usize] = pins;
+                } else {
+                    return Err(Error::NotEnoughPinsLeft);
+                }
+                self.n += 1;
+                Ok(true)
+            } else {
+                Ok(false)
+            }
+        }
+
+        fn score(&self) -> u16 {
+            self.pins.iter().sum()
+        }
+
+        fn score_of_first_roll(&self) -> u16 {
+            self.pins[0]
+        }
+
+        fn end(&self) -> bool {
+            self.n == 2 || self.is_strike()
+        }
+    }
+
+    struct FinalFrame {
+        frame: Frame,
+        extra_rolls: [u16; 2],
+        n: u8,
+        extra: i8,
+    }
+
+    impl FinalFrame {
+        fn new() -> Self {
+            FinalFrame {
+                frame: Frame::new(),
+                extra_rolls: [0; 2],
+                n: 0,
+                extra: -1,
+            }
+        }
+
+        fn record(&mut self, pins: u16) -> Result<(), Error> {
+            if self.extra == -1 {
+                let succ = self.frame.record(pins);
+                match succ {
+                    Err(e) => return Err(e),
+                    Ok(true) => return Ok(()),
+                    Ok(false) => {}
+                }
+
+                if self.frame.is_strike() {
+                    self.extra = 2;
+                } else if self.frame.is_spare() {
+                    self.extra = 1;
+                } else {
+                    self.extra = 0;
+                }
+            }
+
+            if self.n < self.extra as u8 {
+                if self.n == 0 {
+                    self.extra_rolls[self.n as usize] = pins;
+                } else if self.n == 1 {
+                    if self.extra_rolls[0] == 10 || self.extra_rolls[0] + pins <= 10 {
+                        self.extra_rolls[1] = pins;
+                    } else {
+                        return Err(Error::NotEnoughPinsLeft);
+                    }
+                }
+                self.n += 1;
+                Ok(())
+            } else {
+                Err(Error::GameComplete)
+            }
+        }
+
+        fn score(&self) -> u16 {
+            self.frame.score()
+                + if self.extra > 0 {
+                    self.extra_rolls[..self.extra as usize].iter().sum::<u16>()
+                } else {
+                    0
+                }
+        }
+
+        fn score_of_first_roll(&self) -> u16 {
+            self.frame.score_of_first_roll()
+        }
+
+        fn score_of_first_two_rolls(&self) -> u16 {
+            if self.frame.is_strike() {
+                self.frame.score() + self.extra_rolls[0]
+            } else {
+                self.frame.score()
+            }
+        }
+
+        fn game_over(&self) -> bool {
+            self.n == self.extra as u8 || self.frame.end() & self.frame.is_open()
+        }
+    }
+
+    pub struct BowlingGame {
+        frames: [Frame; 9],
+        tenth_frame: FinalFrame,
+        n: u8,
+    }
+
+    impl BowlingGame {
+        pub fn new() -> Self {
+            Self {
+                frames: [Frame::new(); 9],
+                tenth_frame: FinalFrame::new(),
+                n: 0,
+            }
+        }
+
+        pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
+            if pins > 10 {
+                return Err(Error::NotEnoughPinsLeft);
+            }
+            if self.n == 9 {
+                self.tenth_frame.record(pins)?;
+            } else {
+                match self.frames[self.n as usize].record(pins) {
+                    Ok(false) => {
+                        self.n += 1;
+                        if self.n == 9 {
+                            self.tenth_frame.record(pins);
+                        } else {
+                            self.frames[self.n as usize].record(pins);
+                        }
+                    }
+                    Err(e) => return Err(e),
+                    Ok(true) => {}
+                }
+            }
+            Ok(())
+        }
+
+        pub fn score(&self) -> Option<u16> {
+            if self.tenth_frame.game_over() {
+                let mut score = 0;
+                for (i, fr) in self.frames.iter().enumerate() {
+                    score += fr.score();
+                    if fr.is_strike() {
+                        if i + 1 == 9 {
+                            score += self.tenth_frame.score_of_first_two_rolls();
+                        } else {
+                            let next_frame = self.frames[i + 1];
+                            score += next_frame.score();
+                            if next_frame.is_strike() {
+                                if i + 2 == 9 {
+                                    score += self.tenth_frame.score_of_first_roll();
+                                } else {
+                                    let next_frame = self.frames[i + 2];
+                                    score += next_frame.score_of_first_roll();
+                                }
+                            }
+                        }
+                    } else if fr.is_spare() {
+                        if i + 1 == 9 {
+                            score += self.tenth_frame.score_of_first_two_rolls();
+                        } else {
+                            let next_frame = self.frames[i + 1];
+                            score += next_frame.score_of_first_roll();
+                        }
+                    }
+                }
+                score += self.tenth_frame.score();
+                Some(score)
+            } else {
+                None
+            }
+        }
+    }
+} // mod v0
 
 #[cfg(test)]
 mod test {
@@ -457,8 +526,8 @@ mod test {
     }
 
     #[test]
-    fn two_bonus_rolls_after_a_strike_in_the_last_frame_can_score_more_than_10_points_if_one_is_a_strike(
-    ) {
+    fn two_bonus_rolls_after_a_strike_in_the_last_frame_can_score_more_than_10_points_if_one_is_a_strike()
+     {
         let mut game = BowlingGame::new();
         for _ in 0..18 {
             let _ = game.roll(0);
@@ -469,8 +538,8 @@ mod test {
     }
 
     #[test]
-    fn the_second_bonus_rolls_after_a_strike_in_the_last_frame_cannot_be_a_strike_if_the_first_one_is_not_a_strike(
-    ) {
+    fn the_second_bonus_rolls_after_a_strike_in_the_last_frame_cannot_be_a_strike_if_the_first_one_is_not_a_strike()
+     {
         let mut game = BowlingGame::new();
         for _ in 0..18 {
             let _ = game.roll(0);
@@ -502,8 +571,8 @@ mod test {
     }
 
     #[test]
-    fn both_bonus_rolls_for_a_strike_in_the_last_frame_must_be_rolled_before_score_can_be_calculated(
-    ) {
+    fn both_bonus_rolls_for_a_strike_in_the_last_frame_must_be_rolled_before_score_can_be_calculated()
+     {
         let mut game = BowlingGame::new();
         for _ in 0..18 {
             let _ = game.roll(0);
