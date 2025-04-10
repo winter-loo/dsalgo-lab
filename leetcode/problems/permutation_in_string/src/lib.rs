79275e10d52790fe56a7b645076908e1bcc29f49
winter.loo <adjkldd@126.com>
2025-04-10 08:15:32 +0800

    fix

--- leetcode/problems/permutation_in_string/src/lib.rs
+++ leetcode/problems/permutation_in_string/src/lib.rs
@@ -1,10 +1,12 @@
+#![allow(dead_code)]
+
 use std::collections::HashSet;
 
 pub struct Solution;
 
 impl Solution {
     pub fn check_inclusion(s1: String, s2: String) -> bool {
-        let v = permutation_of_heap(&mut s1.chars().collect::<Vec<_>>(), 0, s1.len() - 1);
+        let v = permutation(&mut s1.chars().collect::<Vec<_>>());
         println!("v={v:?}");
         let s: HashSet<_> = v.iter().collect();
         println!("s={s:?}, len={}", s.len());
@@ -13,12 +15,12 @@ impl Solution {
 }
 
 #[allow(dead_code)]
-mod initial {
+pub mod initial {
     // f(abc) = a + f(bc) | b + f(ac) | c + f(ab)
     // f(bc) = b + f(c) | c + f(b)
     // f(ac) = a + f(c) | c + f(a)
     // f(ab) = a + f(b) | b + f(a)
-    fn permutation_collect(input: String) -> Vec<String> {
+    pub fn permutation_collect(input: String) -> Vec<String> {
         assert!(input.chars().all(|c| c.is_ascii_lowercase()));
 
         if input.len() < 2 {
@@ -46,25 +48,112 @@ mod initial {
     }
 }
 
+pub fn permutation(seq: &mut [char]) -> Vec<String> {
+    permutation_of_heap_fixed(seq, 0, seq.len() - 1)
+}
+
+pub fn permutation_of_heap_tail(seq: &mut [char], l: usize, r: usize) -> Vec<String> {
+    permutation_of_heap_fixed(seq, l, r)
+}
+
 /// Heap's permutation algorithm
-pub fn permutation_of_heap(seq: &mut [char], l: usize, r: usize) -> Vec<String> {
+pub fn permutation_of_heap_fixed(seq: &mut [char], l: usize, r: usize) -> Vec<String> {
     if l == r {
         let s = seq.iter().collect();
-        println!("l={l} r={r} s={s}");
+        // println!("l={l} r={r} s={s}");
+        return vec![s];
+    }
+
+    let n = r - l + 1;
+    let mut result = Vec::with_capacity(fact(n));
+    for i in (l..=r).rev() {
+        // println!("i={i} l={l} r={r}");
+        result.extend(permutation_of_heap_fixed(seq, l + 1, r));
+        if n % 2 == 0 {
+            seq.swap(i, l);
+        } else {
+            seq.swap(r, l);
+        }
+    }
+    result
+}
+
+/// No AI can fix this with minimal changes
+/// Heap's permutation algorithm
+pub fn permutation_of_heap_buggy(seq: &mut [char], l: usize, r: usize) -> Vec<String> {
+    if l == r {
+        let s = seq.iter().collect();
+        // println!("l={l} r={r} s={s}");
         return vec![s];
     }
 
     let n = r - l + 1;
     let mut result = Vec::with_capacity(fact(n));
     for i in l..=r {
-        println!("i={i} l={l} r={r}");
-        result.extend(permutation_of_heap(seq, l + 1, r));
+        // println!("i={i} l={l} r={r}");
+        result.extend(permutation_of_heap_buggy(seq, l + 1, r));
         if n % 2 == 0 {
             seq.swap(i, r);
         } else {
-            seq.swap(0, r);
+            seq.swap(l, r);
         }
     }
+    result
+}
+
+// compare with the initial::permutation_collect
+pub fn permutation_of_heap2(seq: &mut [char], l: usize, r: usize) -> Vec<String> {
+    let mut result = Vec::new();
+    
+    if l == r {
+        // Base case: found one permutation
+        let s: String = seq.iter().collect();
+        return vec![s];
+    }
+    
+    for i in l..=r {
+        // Swap elements
+        seq.swap(l, i);
+        
+        // Recursively generate permutations with the first element fixed
+        let mut perms = permutation_of_heap2(seq, l + 1, r);
+        result.append(&mut perms);
+        
+        // Swap back to restore the original sequence
+        seq.swap(l, i);
+    }
+    
+    result
+}
+
+pub fn permutation_of_heap_head(seq: &mut [char], n: usize) -> Vec<String> {
+    println!("permutation of {n}");
+    let mut result = Vec::new();
+    
+    if n == 1 {
+        let s = seq.iter().collect();
+        println!("n=1 s={}", s);
+        result.push(s);
+        return result;
+    }
+    
+    for i in 0..n {
+        // Generate permutations with (n-1) elements
+        let mut perms = permutation_of_heap_head(seq, n - 1);
+        result.append(&mut perms);
+        
+        // If n is odd, swap first and last element
+        // If n is even, swap i-th and last element
+        if n % 2 == 1 {
+            println!("swapping first 0 with last {} where n={n}", n - 1);
+            seq.swap(0, n - 1);
+        } else {
+            println!("swapping i={i} with last {} where n={n}", n - 1);
+            seq.swap(i, n - 1);
+        }
+    }
+    
+    println!("one iteration done where n={n}");
     result
 }
 
@@ -72,7 +161,7 @@ fn fact(n: usize) -> usize {
     (1..=n).fold(1, |p, n| p * n)
 }
 
-pub fn permutation<'a>(
+pub fn permutation_iter<'a>(
     seq: &'a mut [char],
     l: usize,
     r: usize,
@@ -80,10 +169,17 @@ pub fn permutation<'a>(
     PermutationIter {
         seq,
         lefts: vec![l],
-        indicies: vec![l],
+        indicies: vec![],
         r,
-        i: 0,
+        i: r,
     }
+    // PermutationIterOpt {
+    //     seq,
+    //     lefts: vec![l],
+    //     indicies: vec![],
+    //     r,
+    //     i: 0,
+    // }
 }
 
 struct PermutationIter<'a> {
@@ -98,8 +194,9 @@ impl<'a> Iterator for PermutationIter<'a> {
     type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.i <= self.r || !self.indicies.is_empty() {
-            if self.i > self.r {
+        let l = self.lefts.last().copied().unwrap_or(0);
+        if self.i >= l || !self.indicies.is_empty() {
+            if self.i < l {
                 println!("iteration done, return to upper level");
                 self.lefts.pop();
                 self.i = self.indicies.pop().unwrap();
@@ -110,14 +207,14 @@ impl<'a> Iterator for PermutationIter<'a> {
                 let n = self.r - l + 1;
                 println!("swapping where n = {} i = {}", n, self.i);
                 if n % 2 == 0 {
-                    self.seq.swap(self.i, self.r);
+                    self.seq.swap(self.i, l);
                 } else {
-                    self.seq.swap(0, self.r);
+                    self.seq.swap(self.r, l);
                 }
 
-                self.i += 1;
+                self.i = self.i.saturating_sub(1);
                 println!("next i={}", self.i);
-                if n == self.seq.len() && self.i == self.seq.len() {
+                if n == self.seq.len() && self.i == 0 {
                     println!("whole done, indicies={:?}", self.indicies);
                     return None;
                 }
@@ -132,7 +229,7 @@ impl<'a> Iterator for PermutationIter<'a> {
                 // to lower level
                 self.lefts.push(l + 1);
                 // start from new left
-                self.i = l + 1;
+                self.i = self.r;
             }
             // reached to the single element, time to output current permutation
             if let Some(top) = self.lefts.last().copied() {
@@ -149,13 +246,75 @@ impl<'a> Iterator for PermutationIter<'a> {
                 let n = self.r - l + 1;
                 println!("swapping where n={} i={}", n, self.i);
                 if n % 2 == 0 {
-                    self.seq.swap(self.i, self.r);
+                    self.seq.swap(self.i, l);
                 } else {
-                    self.seq.swap(0, self.r);
+                    self.seq.swap(self.r, l);
                 }
 
-                self.i += 1;
+                self.i = self.i.saturating_sub(1);
                 println!("next i={}", self.i);
+                return Some(item);
+            }
+        }
+        None
+    }
+}
+
+struct PermutationIterOpt<'a> {
+    seq: &'a mut [char],
+    lefts: Vec<usize>,
+    indicies: Vec<usize>,
+    r: usize,
+    i: usize,
+}
+
+impl<'a> PermutationIterOpt<'a> {
+    fn handle_level_transition(&mut self) -> bool {
+        // Move up one level
+        self.lefts.pop();
+        self.i = self.indicies.pop().unwrap();
+        
+        // Get current state
+        let l = self.lefts.last().copied().unwrap();
+        let n = self.r - l + 1;
+        
+        // Perform swap based on parity
+        if n % 2 == 0 {
+            self.seq.swap(self.i, self.r);
+        } else {
+            self.seq.swap(0, self.r);
+        }
+        
+        self.i += 1;
+        n == self.seq.len() && self.i == self.seq.len()
+    }
+}
+
+impl<'a> Iterator for PermutationIterOpt<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i <= self.r || !self.indicies.is_empty() {
+            if self.i > self.r {
+                if self.handle_level_transition() {
+                    return None;
+                }
+            }
+
+            // Go down to single element
+            while let Some(l) = self.lefts.last().copied() {
+                if l == self.r {
+                    break;
+                }
+                self.indicies.push(self.i);
+                self.lefts.push(l + 1);
+                self.i = l + 1;
+            }
+
+            // Process current permutation if we've reached a leaf
+            if let Some(_) = self.lefts.last().copied() {
+                let item = self.seq.iter().collect();
+                self.handle_level_transition();
                 return Some(item);
             }
         }
--- leetcode/problems/permutation_in_string/src/main.rs
+++ leetcode/problems/permutation_in_string/src/main.rs
@@ -1,14 +1,23 @@
-use permutation_in_string::{permutation, permutation_of_heap};
+use permutation_in_string::*;
+use std::collections::BTreeSet;
 
 fn main() {
-    let s1 = "abc".to_string();
+    let s1 = "abcd".to_string();
     let mut s: Vec<char> = s1.chars().collect();
 
-    // let v = permutation_of_heap(&mut s, 0, s1.len() - 1);
-    // println!("v={v:?} len={}", v.len());
+    // let v = initial::permutation_collect(s1.clone());
+    // println!("v={v:#?} len={}", v.len());
 
-    let perm = permutation(&mut s, 0, s1.len() - 1);
-    for p in perm {
+    // let v = permutation_of_heap_fixed(&mut s, 0, s1.len() - 1);
+    // println!("v={v:#?} len={}", v.len());
+    // let vs = v.iter().collect::<BtreeSet<_>>();
+    // println!("vs={vs:#?} len={}", vs.len());
+
+    let perm = permutation_iter(&mut s, 0, s1.len() - 1);
+    let v: Vec<_> = perm.collect();
+    for p in v.iter() {
         println!("p={p}");
     }
+    let vs = v.iter().collect::<BTreeSet<_>>();
+    println!("len={}", vs.len());
 }
