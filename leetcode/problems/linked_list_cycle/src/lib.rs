8b050d7f3e5f13fb584653070752ee1f1445197e
winter.loo <adjkldd@126.com>
2025-04-15 22:38:47 +0800

    done linked_list_cycle

--- leetcode/problems/linked_list_cycle/src/lib.rs
+++ leetcode/problems/linked_list_cycle/src/lib.rs
@@ -7,15 +7,48 @@ pub struct ListNode {
 
 impl ListNode {
     #[inline]
-    fn new(val: i32) -> Self {
+    pub fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
 }
 
 pub struct Solution;
-
+//```go
+// type ListNode struct {
+//     Val int
+//     Next *ListNode
+// }
+//
+// func hasCycle(head *ListNode) bool {
+//     fast, slow := head, head
+//     for {
+//         if fast == nil || fast.Next == nil {
+//             return false
+//         }
+//         fast = fast.Next.Next
+//         slow = slow.Next
+//         if fast == slow {
+//             return true;
+//         }
+//     }
+//     return false
+// }
+//```
 impl Solution {
     pub fn has_cycle(head: Option<Box<ListNode>>) -> bool {
-        todo!()
+        let mut slow = &head;
+        let mut fast = &head;
+
+        loop {
+            if fast.is_none() || fast.as_ref().unwrap().next.is_none() {
+                return false;
+            }
+            fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
+            slow = &slow.as_ref().unwrap().next;
+            if fast == slow {
+                return true;
+            }
+        }
+        unreachable!()
     }
 }
