7aa58fed859ceb45d7d02e9d2ef20f4dfe8de9c3
winter.loo <adjkldd@126.com>
2025-04-20 23:38:35 +0800

    done maximum_depth_of_binary_tree

--- leetcode/problems/maximum_depth_of_binary_tree/src/lib.rs
+++ leetcode/problems/maximum_depth_of_binary_tree/src/lib.rs
@@ -1,30 +1,32 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
 pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
 }
 
 impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
-  }
 }
 
 pub struct Solution;
 
 impl Solution {
     pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        // Placeholder implementation
-        0
+        root.map_or(0, |root| {
+            let mut root = root.borrow_mut();
+            1 + Solution::max_depth(root.left.take()).max(Solution::max_depth(root.right.take()))
+        })
     }
 }
