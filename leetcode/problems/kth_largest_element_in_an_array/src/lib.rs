70a13318e7cfdb92edbd7112c747ccad29eb71ef
winter.loo <adjkldd@126.com>
2025-04-22 20:58:21 +0800

    done kth_largest_element_in_an_array

--- leetcode/problems/kth_largest_element_in_an_array/src/lib.rs
+++ leetcode/problems/kth_largest_element_in_an_array/src/lib.rs
@@ -1,8 +1,25 @@
 pub struct Solution;
 
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
 impl Solution {
     pub fn find_kth_largest(nums: Vec<i32>, k: i32) -> i32 {
-        // TODO: Implement the function
-        0
+        assert!((k as usize) <= nums.len());
+
+        let mut heap = BinaryHeap::new();
+        for n in nums {
+            if heap.len() < k as usize {
+                heap.push(Reverse(n));
+            } else {
+                if let Some(Reverse(top)) = heap.peek() {
+                    if n > *top {
+                        heap.pop();
+                        heap.push(Reverse(n));
+                    }
+                }
+            }
+        }
+        return heap.peek().unwrap().0;
     }
 }
