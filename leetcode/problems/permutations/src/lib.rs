ffca7eb63247f0707462c28b7f6c3bf42d1fadbc
winter.loo <adjkldd@126.com>
2025-04-24 14:40:59 +0800

    done permutations

--- leetcode/problems/permutations/src/lib.rs
+++ leetcode/problems/permutations/src/lib.rs
@@ -1,8 +1,19 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        // TODO: Implement the function
-        vec![]
+    pub fn permute(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        if nums.len() <= 1 {
+            return vec![nums];
+        }
+        let mut results = vec![];
+        for i in 0..nums.len() {
+            nums.swap(0, i);
+            let mut subperm = Solution::permute(nums[1..].to_vec());
+            for v in subperm.iter_mut() {
+                v.push(nums[0]);
+            }
+            results.extend(subperm.into_iter());
+        }
+        results
     }
 }
