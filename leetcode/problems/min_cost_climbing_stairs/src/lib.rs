ee3cfcd9c1668172de64043fce79e36712950e97
winter.loo <adjkldd@126.com>
2025-05-06 16:47:01 +0800

    done min_cost_climbing_stairs

--- leetcode/problems/min_cost_climbing_stairs/src/lib.rs
+++ leetcode/problems/min_cost_climbing_stairs/src/lib.rs
@@ -1,7 +1,65 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
-        todo!()
+    // Input: cost = [10,15,20]
+    // Output: 15
+    //
+    // f(4) -> 0
+    // f(3) -> 20
+    // f(2) -> 15 + min(f(3), f(4)) -> 15
+    // f(1) -> 10 + min(f(2), f(3)) -> 10 + 15 -> 25
+    // f(0) -> 0 + min(f(1), f(2)) -> 15
+    //
+    // ideas come from min_cost_climbing_stairs_with_memoization:
+    // current minimum cost always depends on future costs
+    pub fn min_cost_climbing_stairs(costs: Vec<i32>) -> i32 {
+        if costs.len() < 2 {
+            return 0;
+        }
+
+        let mut n1 = 0;
+        let mut n2 = *costs.last().unwrap_or(&0);
+        for i in (0..costs.len() - 1).rev() {
+            let ans = costs[i] + n1.min(n2);
+            n1 = n2;
+            n2 = ans;
+        }
+        n1.min(n2)
+    }
+
+    pub fn min_cost_climbing_stairs_with_memoization(costs: Vec<i32>) -> i32 {
+        // decision tree with memoization
+        fn choose(costs: &[i32], index: isize, mem: &mut [i32]) -> i32 {
+            if index >= costs.len() as isize {
+                return 0;
+            }
+            let next_index = (index + 1) as usize;
+            if mem[next_index] != i32::MAX {
+                return mem[next_index];
+            }
+            let left_cost = choose(costs, index + 1, mem) + costs.get(next_index).unwrap_or(&0);
+            let right_cost = choose(costs, index + 2, mem) + costs.get(next_index + 1).unwrap_or(&0);
+            mem[next_index] = left_cost.min(right_cost);
+            mem[next_index]
+        }
+        let mut mem = vec![i32::MAX; costs.len() + 1];
+        // -1 -> no decision done yet
+        choose(&costs, -1, &mut mem)
+    }
+
+    // see https://excalidraw.com/#json=yP7yj0Wd4-Ryo7RdjZ50x,ud1_H_5bUSeUd3yVXiHxeg
+    pub fn min_cost_climbing_stairs_brute_force(costs: Vec<i32>) -> i32 {
+        // Time Limit Exceeded
+        // decision tree without pruning
+        fn choose(costs: &[i32], index: isize) -> i32 {
+            if index >= costs.len() as isize {
+                return 0;
+            }
+            let left_cost = choose(costs, index + 1) + costs.get((index + 1) as usize).unwrap_or(&0);
+            let right_cost = choose(costs, index + 2) + costs.get((index + 2) as usize).unwrap_or(&0);
+            left_cost.min(right_cost)
+        }
+        // -1 -> no decision done yet
+        choose(&costs, -1)
     }
 }
--- leetcode/problems/min_cost_climbing_stairs/tests/basic.rs
+++ leetcode/problems/min_cost_climbing_stairs/tests/basic.rs
@@ -5,6 +5,11 @@ fn test_example_1() {
     // Input: cost = [10,15,20]
     // Output: 15
     let cost = vec![10, 15, 20];
+    // f(4) -> 0
+    // f(3) -> 20
+    // f(2) -> 15 + min(f(3), f(4)) -> 15
+    // f(1) -> 10 + min(f(2), f(3)) -> 10 + 15 -> 25
+    // f(0) -> 0 + min(f(1), f(2)) -> 15
     assert_eq!(Solution::min_cost_climbing_stairs(cost), 15);
 }
 
@@ -27,12 +32,12 @@ fn test_minimal_case() {
 fn test_equal_cost() {
     // All steps have the same cost
     let cost = vec![1, 1, 1, 1];
-    assert_eq!(Solution::min_cost_climbing_stairs(cost), 1);
+    assert_eq!(Solution::min_cost_climbing_stairs(cost), 2);
 }
 
 #[test]
 fn test_alternating_costs() {
     // Alternating high and low costs
     let cost = vec![10, 1, 10, 1, 10];
-    assert_eq!(Solution::min_cost_climbing_stairs(cost), 3);
+    assert_eq!(Solution::min_cost_climbing_stairs(cost), 2);
 }
