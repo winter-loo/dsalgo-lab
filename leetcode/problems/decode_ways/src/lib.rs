ecaf8a8f7bba7c041c04b4dad23f0ba8535ab39f
winter.loo <adjkldd@126.com>
2025-05-14 08:41:47 +0800

    doing decode_ways

--- leetcode/problems/decode_ways/src/lib.rs
+++ leetcode/problems/decode_ways/src/lib.rs
@@ -10,6 +10,24 @@ impl Solution {
 }
 
 // Time Limit Exceeded version
+// https://excalidraw.com/#json=jFRTgrklo5mLYgWSL9b_4,YCYDUIuUWbJNNAtxf0WYMQ
+fn traverse_with_memoization(s: &[char], index: usize, sum: u32, count: &mut usize) {
+    println!("index={index} sum={sum}");
+    if index >= s.len() {
+        if sum > 0 && sum <= 26 {
+            *count += 1;
+        }
+        return;
+    }
+    let sum = s[index].to_digit(10).map(|n| sum * 10 + n).unwrap_or(0);
+    if sum <= 0 || sum > 26 {
+        return;
+    }
+    traverse(s, index + 1, sum, count);
+    traverse(s, index + 1, 0, count);
+}
+
+// Time Limit Exceeded version
 // https://excalidraw.com/#json=18SkzARuhP4oxVhFqGyN_,0lMgdgeWP4wXR2thy9ofBg
 fn traverse(s: &[char], index: usize, sum: u32, count: &mut usize) {
     println!("index={index} sum={sum}");
