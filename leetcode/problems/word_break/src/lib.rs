74023af876aa2cf8159cd3fb4b4332cce9e0d490
winter.loo <adjkldd@126.com>
2025-05-18 21:04:01 +0800

    done word_break

--- leetcode/problems/word_break/src/lib.rs
+++ leetcode/problems/word_break/src/lib.rs
@@ -1,7 +1,43 @@
 pub struct Solution;
 
 impl Solution {
+    // Input: s = "leetcode", wordDict = ["leet","code"]
+    //
+    // 7    false
+    // 6    false
+    // 5    false
+    // 4    true
+    // 3    false
+    // 2    false
+    // 1    false
+    // 0    true
+    //
+    // Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    //
+    // 8    g           false
+    // 7    og          false
+    // 6    dog         true
+    // 5    ndog        false
+    // 4    andog       false
+    // 3    sandog      false
+    // 2    tsandog     false
+    // 1    atsandog    false
+    // 0    catsandog   false|false
     pub fn word_break(s: String, word_dict: Vec<String>) -> bool {
-        todo!()
+        let mut dp = vec![false; s.len()];
+        for i in (0..s.len()).rev() {
+            for word in &word_dict {
+                // println!("i={i} s[i..]={} word={word}", &s[i..]);
+                if s[i..].starts_with(&word[..]) {
+                    // println!("dp[i={i}]={}, next_i={}", dp[i], i + word.len());
+                    dp[i] |= if i + word.len() < s.len() {
+                        dp[i + word.len()]
+                    } else {
+                        true
+                    };
+                }
+            }
+        }
+        dp[0]
     }
 }
