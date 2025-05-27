467b7a941beb611d5e7b5e25b0b4e97a600445ff
winter.loo <adjkldd@126.com>
2025-05-27 21:18:20 +0800

    done partition_equal_subset_sum

--- leetcode/problems/partition_equal_subset_sum/src/lib.rs
+++ leetcode/problems/partition_equal_subset_sum/src/lib.rs
@@ -1,7 +1,37 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn can_partition(nums: Vec<i32>) -> bool {
-        todo!()
+    // [1,5,11,5]
+    // [1]      |    [5,11,5]
+    // [5]      |    [1,11,5]
+    // [11]     |    [1,5,5]
+    // [5]      |    [1,5,11]
+    //
+    // [1] [5]      |       [11,5]
+    // [1] [11]     |       [5,5]
+    // [1] [5]      |       [11,5]
+    //
+    // [1] [5] [11]     |       [5]
+    // [1] [5] [5]      |       [11]
+    //
+    // is there existing `i` such that sum(0..i) == sum(i..n) ?
+    pub fn can_partition(mut nums: Vec<i32>) -> bool {
+        fn recur(nums: &mut [i32], left_sum: i32) -> bool {
+            if nums.iter().sum::<i32>() == left_sum {
+                return true;
+            }
+            if nums.len() <= 1 {
+                return false;
+            }
+            for i in 0..nums.len() {
+                nums.swap(i, 0);
+                let n = nums[0];
+                if recur(&mut nums[1..], left_sum + n) {
+                    return true;
+                }
+            }
+            false
+        }
+        recur(&mut nums, 0)
     }
 }
