d36a991a6f21164a2e5f1a2d0c923c5b6a3b2556
winter.loo <adjkldd@126.com>
2025-06-09 20:29:35 +0800

    doing target_sum

--- leetcode/problems/target_sum/src/lib.rs
+++ leetcode/problems/target_sum/src/lib.rs
@@ -8,17 +8,17 @@ impl Solution {
     //
     // S=1,1,1,1,1 T=3
     // f(t,i) = f(t-S[i],i+1) + f(t+S[i],i+1) for i in [0,len(S)] and target is 't'.
-    // or
-    // f(t,i) = f(t+S[i-1],i-1) + f(t-S[i-1],i-1), t-S[i] < t < t+S[i]
+    // The answer is `f(T,0)` which means staring from the index 0 of S, the number
+    // of ways to sum to T.
     //
-    // S\T      0   1   2   3   4
-    //      +----------------
-    //   0  |   1   0   0   0   0
-    // 1 1  |   0   1   0   0   0
-    // 1 2  |   1   0   1   0   0
-    // 1 3  |   0   1   0   1
-    // 1 4  |       0
-    // 1 5  |       0
+    // S\T   -1 | 0   1   2   3 | 4
+    //      +---|---------------|--
+    // 1 5  | 0 | 0   1   0   0 | 0
+    // 1 4  | 0 | 1   0   1   0 | 0
+    // 1 3  | 0 | 0   2   0   1 | 0
+    // 1 2  | 0 | 2   0   3   0 | 0
+    // 1 1  | 0 | 0   5   0   3 | 0
+    //
     pub fn find_target_sum_ways(nums: Vec<i32>, target: i32) -> i32 {
         todo!()
     }
