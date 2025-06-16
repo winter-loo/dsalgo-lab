71ebdc170158271dd8a1f36cf0761ab67ed0a321
winter.loo <adjkldd@126.com>
2025-06-16 20:46:22 +0800

    doing longest_increasing_path_in_a_matrix

--- leetcode/problems/longest_increasing_path_in_a_matrix/src/lib.rs
+++ leetcode/problems/longest_increasing_path_in_a_matrix/src/lib.rs
@@ -1,6 +1,27 @@
 pub struct Solution;
 
 impl Solution {
+    // Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+    // 9,9,4
+    // 6,6,8
+    // 2,1,1
+    //
+    // First thought, use BFS to explore every path. O((m*n)^2)
+    //
+    // (0,0) -> (0,1) ❌
+    // (0,0) -> (1,0) ❌
+    //
+    // (0,1) -> (0,0) ❌
+    // (0,1) -> (0,2) ❌
+    // (0,1) -> (1,1) ❌
+    //
+    // (0,2) -> (0,1) ✅
+    // (0,2) -> (1,2) ✅
+    //
+    // (1,0) -> (0,0) ✅
+    // (1,0) -> (1,1) ❌
+    // (1,0) -> (2,0) ❌
+    //
     pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
         todo!()
     }
