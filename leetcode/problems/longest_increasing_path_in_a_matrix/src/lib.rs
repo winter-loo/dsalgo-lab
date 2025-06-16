8df206c75bde21b48183f29fb45772188dfc89cb
winter.loo <adjkldd@126.com>
2025-06-17 05:04:07 +0800

    doing longest_increasing_path_in_a_matrix

--- leetcode/problems/longest_increasing_path_in_a_matrix/src/lib.rs
+++ leetcode/problems/longest_increasing_path_in_a_matrix/src/lib.rs
@@ -22,6 +22,23 @@ impl Solution {
     // (1,0) -> (1,1) ❌
     // (1,0) -> (2,0) ❌
     //
+    // For any position, (r,c),
+    // if M(r,c-1) > M(r,c), then f(r,c) = 1 + f(r,c-1) // left
+    // if M(r-1,c) > M(r,c), then f(r,c) = 1 + f(r-1,c) // top
+    // if M(r,c+1) > M(r,c), then f(r,c) = 1 + f(r,c+1) // right
+    // if M(r+1,c) > M(r,c), then f(r,c) = 1 + f(r+1,c) // bottom
+    //
+    // 1,2,3
+    // 6,5,4
+    // 7,8,9
+    //
+    // 1 ┌─> 2 ┌─> 3 ──> 4 ┌─> 5 ┌─> 6 ──> 7 ──> 8 ──> 9
+    //   │     │           │     └─> 8 ──> 9
+    //   │     │           └─> 9
+    //   │     └─> 5 ┌─> 6 ──> 7 ──> 8 ──> 9
+    //   │           └─> 8 ──> 9
+    //   └─> 6 ──> 7 ──> 8 ──> 9
+    //
     pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
         todo!()
     }
