2134a10afeaae3c3265318d597c0758068d6893f
winter.loo <adjkldd@126.com>
2025-06-17 20:20:31 +0800

    done longest_increasing_path_in_a_matrix

--- leetcode/problems/longest_increasing_path_in_a_matrix/src/lib.rs
+++ leetcode/problems/longest_increasing_path_in_a_matrix/src/lib.rs
@@ -1,6 +1,74 @@
 pub struct Solution;
 
 impl Solution {
+    pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
+        Solution::longest_increasing_path_with_mem(matrix)
+    }
+
+    pub fn longest_increasing_path_with_mem(matrix: Vec<Vec<i32>>) -> i32 {
+        /// return the maximum depth from (row, col) of matrix
+        fn dfs(
+            matrix: &[&[i32]],
+            row: usize,
+            col: usize,
+            seen: &mut Vec<Vec<bool>>,
+            mem: &mut Vec<Vec<usize>>,
+        ) -> usize {
+            let dirs = [[0, -1], [-1, 0], [0, 1], [1, 0]];
+            let mut local_max_depth = 1;
+            for dir in dirs {
+                let next_row = row as isize + dir[0];
+                let next_col = col as isize + dir[1];
+
+                // boundary testing
+                if next_row < 0
+                    || next_row >= matrix.len() as isize
+                    || next_col < 0
+                    || next_col >= matrix[0].len() as isize
+                {
+                    continue;
+                }
+
+                let next_row = next_row as usize;
+                let next_col = next_col as usize;
+
+                // visibility testing
+                if seen[next_row][next_col] {
+                    continue;
+                }
+                seen[next_row][next_col] = true;
+
+                if matrix[next_row][next_col] > matrix[row][col] {
+                    let local_detph = if mem[next_row][next_col] != 0 {
+                        1 + mem[next_row][next_col]
+                    } else {
+                        1 + dfs(matrix, next_row, next_col, seen, mem)
+                    };
+
+                    // println!("row={row} col={col} next_row={next_row} next_col={next_col} local_detph={local_detph}");
+                    local_max_depth = local_max_depth.max(local_detph);
+                }
+                seen[next_row][next_col] = false; // backtrack
+            }
+            mem[row][col] = local_max_depth;
+            // println!("mem=");
+            // for v in mem {
+            //     println!("{v:?}");
+            // }
+            local_max_depth
+        }
+        let m: Vec<&[i32]> = matrix.iter().map(|row| row.as_slice()).collect();
+        let mut ans = 1;
+        let mut mem = vec![vec![0usize; matrix[0].len()]; matrix.len()];
+        for row in 0..matrix.len() {
+            for col in 0..matrix[0].len() {
+                let mut seen = vec![vec![false; matrix[0].len()]; matrix.len()];
+                ans = ans.max(dfs(&m, row, col, &mut seen, &mut mem));
+            }
+        }
+        ans as i32
+    }
+
     // Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
     // 9,9,4
     // 6,6,8
@@ -39,7 +107,61 @@ impl Solution {
     //   │           └─> 8 ──> 9
     //   └─> 6 ──> 7 ──> 8 ──> 9
     //
-    pub fn longest_increasing_path(matrix: Vec<Vec<i32>>) -> i32 {
-        todo!()
+    // However, from the above tree graph, we could know DFS should be used
+    // and with memoization we can avoid the subproblems recomputing overhead.
+    //
+    // Time Limit Exceeded version
+    pub fn longest_increasing_path_dfs(matrix: Vec<Vec<i32>>) -> i32 {
+        fn dfs(
+            matrix: &[&[i32]],
+            row: usize,
+            col: usize,
+            depth: usize,
+            max_depth: &mut usize,
+            seen: &mut Vec<Vec<bool>>,
+        ) -> usize {
+            let dirs = [[0, -1], [-1, 0], [0, 1], [1, 0]];
+            for dir in dirs {
+                let next_row = row as isize + dir[0];
+                let next_col = col as isize + dir[1];
+
+                // boundary testing
+                if next_row < 0
+                    || next_row >= matrix.len() as isize
+                    || next_col < 0
+                    || next_col >= matrix[0].len() as isize
+                {
+                    continue;
+                }
+
+                let next_row = next_row as usize;
+                let next_col = next_col as usize;
+
+                // visibility testing
+                if seen[next_row][next_col] {
+                    continue;
+                }
+                seen[next_row][next_col] = true;
+
+                if matrix[next_row][next_col] > matrix[row][col] {
+                    let d = dfs(matrix, next_row, next_col, depth + 1, max_depth, seen);
+                    *max_depth = (*max_depth).max(d);
+                }
+                seen[next_row][next_col] = false; // backtrack
+            }
+            depth
+        }
+        let m: Vec<&[i32]> = matrix.iter().map(|row| row.as_slice()).collect();
+        let mut ans = 1;
+        for row in 0..matrix.len() {
+            for col in 0..matrix[0].len() {
+                let mut max_depth = 1;
+                let mut seen = vec![vec![false; matrix[0].len()]; matrix.len()];
+                dfs(&m, row, col, 1, &mut max_depth, &mut seen);
+                // dfs(&m, 0, 0, 1, &mut max_depth, &mut seen); // for debug
+                ans = ans.max(max_depth);
+            }
+        }
+        ans as i32
     }
 }
--- leetcode/problems/longest_increasing_path_in_a_matrix/tests/basic.rs
+++ leetcode/problems/longest_increasing_path_in_a_matrix/tests/basic.rs
@@ -5,6 +5,9 @@ fn test_example_1() {
     // Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
     // Output: 4
     // Explanation: The longest increasing path is [1, 2, 6, 9].
+    // [9, 9, 4]
+    // [6, 6, 8]
+    // [2, 1, 1]
     let matrix = vec![vec![9, 9, 4], vec![6, 6, 8], vec![2, 1, 1]];
     assert_eq!(Solution::longest_increasing_path(matrix), 4);
 }
@@ -29,15 +32,21 @@ fn test_example_3() {
 #[test]
 fn test_decreasing_matrix() {
     // Decreasing matrix
+    // [9, 8, 7]
+    // [6, 5, 4]
+    // [3, 2, 1]
     let matrix = vec![vec![9, 8, 7], vec![6, 5, 4], vec![3, 2, 1]];
-    assert_eq!(Solution::longest_increasing_path(matrix), 1);
+    assert_eq!(Solution::longest_increasing_path(matrix), 5);
 }
 
 #[test]
 fn test_increasing_matrix() {
     // Increasing matrix
+    // [1,2,3]
+    // [4,5,6]
+    // [7,8,9]
     let matrix = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
-    assert_eq!(Solution::longest_increasing_path(matrix), 9);
+    assert_eq!(Solution::longest_increasing_path(matrix), 5);
 }
 
 #[test]
@@ -50,4 +59,20 @@ fn test_spiral_matrix() {
         vec![10, 9, 8, 7],
     ];
     assert_eq!(Solution::longest_increasing_path(matrix), 16);
+}
+
+#[test]
+fn test_example_4() {
+    let matrix = vec![vec![1, 2, 3], vec![6, 5, 4], vec![7, 8, 9]];
+    assert_eq!(Solution::longest_increasing_path(matrix), 9);
+}
+
+#[test]
+fn test_example_5() {
+    // [1,4,7,9]
+    // [0,3,8,5]
+    // [3,6,0,6]
+    // [1,4,5,6]
+    let matrix = vec![vec![1,4,7,9], vec![0,3,8,5], vec![3,6,0,6], vec![1,4,5,6]];
+    assert_eq!(Solution::longest_increasing_path(matrix), 5);
 }
