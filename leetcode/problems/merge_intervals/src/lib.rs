4be532bf3170556451b2036b5d09c74ec105636a
winter.loo <adjkldd@126.com>
2025-07-04 08:34:38 +0800

    doing merge_intervals

--- leetcode/problems/merge_intervals/src/lib.rs
+++ leetcode/problems/merge_intervals/src/lib.rs
@@ -1,9 +1,13 @@
 pub struct Solution;
 
 impl Solution {
-    // LeetCode 56: Merge Intervals
-    // Function to merge all overlapping intervals
     pub fn merge(intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        todo!()
+        if intervals.len() < 2 {
+            return intervals;
+        }
+        let mut answer = vec![intervals[0].clone()];
+        for intr in intervals[1..].into_iter() {
+        }
+        answer
     }
 }
