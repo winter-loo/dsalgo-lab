830327ca4f97978085977d51b36ab5fe1fbeefed
winter.loo <adjkldd@126.com>
2025-07-07 19:51:41 +0800

    done single_number

--- leetcode/problems/single_number/src/lib.rs
+++ leetcode/problems/single_number/src/lib.rs
@@ -1,9 +1,9 @@
 pub struct Solution;
 
 impl Solution {
-    // LeetCode 136: Single Number
-    // Function to find the single element in an array where every element appears twice except for one
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        todo!()
+        nums.iter().fold(0, |acc, v| {
+            acc ^ v
+        })
     }
 }
